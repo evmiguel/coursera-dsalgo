77d020b4c9ebbddd1c238fd4283ecdfccb90d612
Erika Miguel <ms.erikamiguel@gmail.com>
2018-12-11 12:16:44 -0500

    node implementation of primitive calculator

--- week5/primitive.js
+++ week5/primitive.js
@@ -30,5 +30,49 @@ function readLine(line) {
 }
 
 function primitive(n){
-  if (n === 1) { return { steps: 0, intermediate: [1] } }
+  if (n === 1) {
+    return {
+      steps: 0,
+      intermediate: [1]
+    }
+  }
+  let hops = [0]
+  hops[1] = 1
+
+  for(let i = 2; i <= n; i++){
+    let indices = [i-1]
+    if (i % 2 === 0){
+      indices.push(i/2)
+    }
+    if (i % 3 === 0){
+      indices.push(i/3)
+    }
+
+    minHops = Math.min(...indices)
+    hops[i] = minHops + 1
+  }
+
+  let ptr = n
+  let optimal = [ptr]
+
+  while (ptr != 1){
+    let candidates = [ptr -1]
+    if (ptr % 2 === 0){
+      candidates.push(ptr / 2)
+    }
+    if (ptr % 3 === 0){
+      candidates.push(ptr / 3)
+    }
+
+    let iHop = candidates.map(c => [c, hops[c]])
+    let minHop = Math.min(...iHop.map(c => c[1]))
+    ptr = iHop.filter(c => c[1] === minHop).pop()[0]
+    optimal.push(ptr)
+  }
+
+  return {
+    steps: optimal.length,
+    intermediate: optimal.reverse()
+  }
+
 }
